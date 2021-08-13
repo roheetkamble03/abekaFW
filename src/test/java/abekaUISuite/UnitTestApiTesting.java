7472b3efe0083ea4764ab2d56c6d2a9277a88a26
Roheet <rohit.kamble@pcci.edu>
2021-08-13 21:39:09 +0530

    API

--- src/test/java/abekaUISuite/UnitTestApiTesting.java
+++ src/test/java/abekaUISuite/UnitTestApiTesting.java
@@ -17,8 +17,13 @@ public class UnitTestApiTesting extends GenericAction {
         Response response = given().when().queryParam("id","100").when().get(endURL);
         List<DigitalAssessmentPojo> digitalAssessmentPojoList = response.getBody().as(new TypeRef<List<DigitalAssessmentPojo>>(){});
         DigitalAssessmentPojo digitalAssessmentPojo = digitalAssessmentPojoList.get(0);
+
+        softAssertions.assertThat(digitalAssessmentPojo.getStudentid().equals("100"))
+                .as("StudentId is not equal to 100").isTrue();
         softAssertions.assertThat(digitalAssessmentPojo.getShort_description().equals("Hebrew History"))
                 .as("Short description is not equal to Hebrew History").isTrue();
+        softAssertions.assertThat(digitalAssessmentPojo.getApref().equals("1257222"))
+                .as("Application number is not equal to 1257222").isTrue();
 
        response = given().when().when().get(endURL);
        softAssertions.assertThat(response.getBody().asString().equals("{\"Message\":\"Studentid is mandatory.\"}"))
